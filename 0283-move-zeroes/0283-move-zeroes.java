370f4d8ccd88a0e80377daede612a67c6c65f84a
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-01-08 21:03:54 +0530

    Time: 2 ms (85.22%), Space: 46.1 MB (40.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,34 +1,24 @@
 import java.util.Scanner;
-
 class Solution {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] nums = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        // Call moveZeroes if array has more than 1 element
+        for (int i = 0; i < n; i++) {nums[i] = sc.nextInt();}
         moveZeroes(nums);
-
-        // Print the modified array
         for (int i = 0; i < n; i++) {
-            System.out.print(nums[i] + " "); // Print all on one line
+            System.out.print(nums[i] + " "); 
         }
-        System.out.println(); // Newline after printing all elements
-    }
+        System.out.println(); }
 
     public static void moveZeroes(int[] nums) {
-        int j = 0; // Pointer to the position for the next non-zero element
+        int j = 0; 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                // Swap non-zero element with the next position at j
                 int temp = nums[j];
                 nums[j] = nums[i];
                 nums[i] = temp;
-                j++; // Move the pointer for the next non-zero element
+                j++; 
             }
         }
     }
