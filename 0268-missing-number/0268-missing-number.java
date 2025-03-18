e80952a94d9e0752188af5fdf7bee4aac5bd1c7e
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-03-18 22:14:42 +0530

    Time: 0 ms (100%), Space: 45 MB (96.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,29 +1,14 @@
 class Solution {
 
-    public static void main (String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [] nums = new int [n];
-
-        for (int i=0; i<n; i++){
-            nums[i] = sc.nextInt();
-        }
-
-        System.out.print(missingNumber(nums));
-    }
+    
     public static int missingNumber(int[] nums) {
         int n = nums.length;
-        int sumOfNum = 0;
-        int sum = 0;
-        
-        
-        for(int i = 0; i<n ; i++){
-            sum = sum + nums[i];
+        int s = (n*(n+1))/2;
+        int m = 0;
+        for (int i = 0; i < n; i++){
+            m = m + nums[i];
         }
-        sumOfNum = (n*(n+1))/2;
-
-        return(sumOfNum - sum);
-        
-      
+        int temp = s - m;
+        return temp;
     }
 }
