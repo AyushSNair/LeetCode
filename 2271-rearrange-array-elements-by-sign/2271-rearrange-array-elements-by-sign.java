99253f96a41ef141723d4537b73a31874ba4b489
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-06-04 16:40:40 +0530

    Time: 3 ms (100%), Space: 77.4 MB (38.02%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int [] arr1 = new int[nums.length/2];
-        int [] arr2 = new int[nums.length/2];
-        int [] arr3 = new int[nums.length];
-        int k = 0;
-        for (int i = 0; i< nums.length; i++){
-            if(nums[i] > -1){
-                arr1[k] = nums[i];
-                k++;
+        int pos = 0;
+        int neg = 1;
+        int [] arr = new int [nums.length];
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] > 0){
+                arr[pos] = nums[i];
+                pos = pos + 2;
             }
-        }
-        k = 0;
-        for (int i = 0; i< nums.length; i++){
             if(nums[i] < 0){
-                arr2[k] = nums[i];
-                k++;
+                arr[neg] = nums[i];
+                neg = neg + 2;
             }
-        }      
-
-        
-        for (int i = 0; i < arr1.length; i++){
-            
-            arr3[i*2 ] = arr1[i];
-            arr3[i*2 + 1] = arr2[i];
         }
-
-        return arr3;
+        return arr;
     }
 }
