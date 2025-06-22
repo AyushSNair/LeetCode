a519c8b4a11e6a8969dc13e8cd8a1aa78af67054
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-06-22 09:35:11 +0530

    Time: 4 ms (28.01%), Space: 12.6 MB (20.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -6,23 +6,23 @@ class Solution(object):
         """
         # converting string to list
         stk = []
-        if len(s) == 1:
-            return False
+
         for ele in s:
-            if ele == '}' or ele == ')' or ele == ']':
+            if ele == '{' or ele == '[' or ele == '(':
+                stk.append(ele)
+            else:
                 if len(stk) == 0:
                     return False
-            if ele == '{' or ele == '(' or ele == '[':
-                stk.append(ele)
-            elif ele == '}' and stk[-1] == '{':
-                stk.pop()
-            elif ele == ')' and stk[-1] == '(':
-                stk.pop()
-            elif ele == ']' and stk[-1] == '[':
-                stk.pop()
-            else:
-                return False
+                elif ele == ')' and stk[-1] == '(':
+                    stk.pop()
+                elif ele == ']' and stk[-1] == '[':
+                    stk.pop()
+                elif ele == '}' and stk[-1] == '{':
+                    stk.pop()
+                else:
+                    return False
+            
         if len(stk) == 0:
             return True
-        else:
+        else: 
             return False
