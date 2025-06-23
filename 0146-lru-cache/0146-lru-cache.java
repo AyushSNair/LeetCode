addbab98461d6727f8003f3f47c767e5eff3588b
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-06-23 19:32:32 +0530

    Time: 52 ms (25.25%), Space: 115.3 MB (42.52%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,65 @@
+import java.util.*;
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }    
+    HashMap<Integer, Node> mpp = new HashMap<Integer, Node>();
+    int cap = 0;
+    Node head = new Node(0,0);
+    Node tail = new Node(0,0);
+    public LRUCache(int capacity) {
+        cap = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if(mpp.containsKey(key)){
+            Node node = mpp.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        if(mpp.containsKey(key)){
+            remove(mpp.get(key));
+        }
+        if(mpp.size() == cap){
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+
+    }
+
+    public void remove(Node node){
+        mpp.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    public void insert(Node node){
+        mpp.put(node.key, node);
+        node.next = head.next;
+        node.prev = head;
+        head.next = node;
+        node.next.prev = node;
+    }   
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
