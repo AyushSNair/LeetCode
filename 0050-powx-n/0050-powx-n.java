7d4a7a790d423592f17c6f6a4d138e3e9e750f19
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-07-12 21:19:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (68.28%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,34 @@
+class Solution {
+    public static double myPow(double x, int n) {
+        double ans = 1.0;
+        long nn = n;
+
+        if(n < 0){
+            nn = (-1) * nn;
+        }
+        while(nn > 0){
+            if(nn % 2 == 0){
+                x = x*x;
+                nn = nn/2;
+            }
+            else{
+                ans = ans * x;
+                nn = nn -1;
+            }
+
+        }
+
+        if(n < 0){
+            ans = 1/ans;
+        }
+        return ans;
+    }
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        double x =sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.print(myPow(x,n));
+    }
+}
