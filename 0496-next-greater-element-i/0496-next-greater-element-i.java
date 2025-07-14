5554f16fcfafc84b54942a7dca601cd1ce147a9a
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-07-14 23:32:12 +0530

    Time: 5 ms (17.79%), Space: 43.2 MB (91.77%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,31 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int [] arr = new int[nums1.length];
-
-        for(int i = 0; i<nums1.length; i++){
-            boolean flag = false;
-            int index = 0;
-            for (int j = 0; j<nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    index = j;
-                    flag = true;
-                    break;
+        int n = nums1.length;
+        int[] arr = new int[n];
+        boolean flag = false;
+        for(int i = 0; i < nums1.length; i++){
+            int j = 0;
+            flag = false;
+            for(j = 0; j < nums2.length; j++){
+                if(nums2[j] == nums1[i]){
+                    for(int k = j+1; k < nums2.length; k++){
+                        if(nums2[k] > nums1[i]){
+                            arr[i] = nums2[k];
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(flag == false){
+                        arr[i] = -1;
+                    }
                 }
             }
-            boolean greaterFound = false;
-            for(int j = index + 1; j<nums2.length; j++){
-                if(nums2[j] > nums1[i]){
-                    arr[i] = nums2[j];
-                    greaterFound = true;
-                    break;
-                }
-
-            }
-
-            if(greaterFound == false){
-                arr[i] = -1;
-            }
-        } 
+        }
         return arr;
     }
 }
