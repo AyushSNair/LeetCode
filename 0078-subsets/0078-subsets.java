8a89e54b17e8c49742b2bb534767bf5cb77a89ed
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-07-24 23:02:24 +0530

    Time: 0 ms (100%), Space: 43.1 MB (10.04%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,20 +1,41 @@
 class Solution {
+
+    public List<List<Integer>> solve(ArrayList<Integer> ip, ArrayList<Integer> op, List<List<Integer>> arrList){
+        if(ip.size() == 0){
+            arrList.add(new ArrayList<>(op));
+            return arrList;
+        }
+
+        ArrayList<Integer> op1 = new ArrayList<>(op);
+        ArrayList<Integer> op2 = new ArrayList<>(op);
+    
+
+        op2.add(ip.get(0));
+        ArrayList<Integer> newIp = new ArrayList<>(ip);
+        newIp.remove(0);
+
+        solve(newIp,op1,arrList);
+        solve(newIp,op2,arrList);
+
+        return arrList;
+    }
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> arr = new ArrayList<>();
-        int n = nums.length;
+        
 
-        for(int i = 0; i < 1<<n ; i++){
-            ArrayList<Integer> ans = new ArrayList<Integer>();
-            for(int j = 0; j < n; j++){
-                if((i & (1<<j )) != 0 ){
-                    ans.add(nums[j]);
-
-                }
-                
-            }
-            arr.add(ans);
+        List<List<Integer>> arrList = new ArrayList<>();
+        if(nums.length == 0){
+            return arrList;
         }
         
-        return arr;
+
+        ArrayList<Integer> ip = new ArrayList<>();
+        for (int num : nums) {
+            ip.add(num);
+        }
+        ArrayList<Integer> op = new ArrayList<>();
+
+        solve(ip,op,arrList);
+        return arrList;
     }
 }
