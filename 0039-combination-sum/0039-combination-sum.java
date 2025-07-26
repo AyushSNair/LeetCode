77b40720abfbc16e7844d5f25a3ab23552e3ce25
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-07-26 20:45:23 +0530

    Time: 9 ms (5.18%), Space: 45.7 MB (6.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,38 @@
 class Solution {
-
-    private void findCombinationSum(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer>ds){
-        if(ind == arr.length){
-            if(target == 0){
-                ans.add(new ArrayList<> (ds));
-            }
-            return;
-        }
-
-        if(arr[ind] <= target){
-            ds.add(arr[ind]);
-            findCombinationSum(ind, arr, target - arr[ind], ans, ds);
-            ds.remove(ds.size()-1);
-        }
-        findCombinationSum(ind+1, arr, target, ans, ds);
-    }
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombinationSum(0, candidates, target, ans, new ArrayList<>());
-        return ans;
+        List<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> ip = new ArrayList<>();
+        for(int i = 0 ; i < candidates.length; i++){
+            ip.add(candidates[i]);
+        }
+
+        ArrayList<Integer> op = new ArrayList<>();
+        solve(result, ip, op, target);
+
+        return result;
     }
 
-    
-    
+    public List<List<Integer>> solve(List<List<Integer>> result, ArrayList<Integer> ip, ArrayList<Integer> op, int target){
+        int target1 = target;
+        int target2 = target;
+        // Base condition
+        if(target1 == 0 || target2 == 0){
+            result.add(new ArrayList<>(op));
+            return result;
+        }
+        if(target1 < 0 || ip.isEmpty()){
+            return result;
+        }
+
+        ArrayList<Integer> op1 = new ArrayList<>(op);
+        op1.add(ip.get(0));
+        solve(result, ip, op1, target - ip.get(0));
+
+
+        ArrayList<Integer> newIp = new ArrayList<>(ip);
+        newIp.remove(0);
+        solve(result, newIp, op, target);
+
+        return result;
+    }
 }
