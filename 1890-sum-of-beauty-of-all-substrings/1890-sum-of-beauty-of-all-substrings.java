2b560634d4bd9883c20a8a19095b09a00fa63d81
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-08-17 23:11:18 +0530

    Time: 88 ms (23.52%), Space: 42.7 MB (86.73%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -1,38 +1,40 @@
 import java.util.*;
 class Solution {
-    public int minFreq(int[] freq){
-        int minCount = Integer.MAX_VALUE;
-        for(int i = 0; i < 26; i++){
-            if(freq[i] != 0){
-                minCount = Math.min(minCount, freq[i]);
-            }
-        }
-        return minCount;
-    }
+
     public int maxFreq(int[] freq){
-        int maxCount = 0;
-        for(int i = 0; i <26; i++){
+        int maxEle = Integer.MIN_VALUE;
+        for(int i = 0; i < freq.length; i++){
             if(freq[i] != 0){
-                maxCount = Math.max(maxCount,freq[i]);
+                maxEle = Math.max(maxEle, freq[i]);
             }
-            
         }
-        return maxCount;
+        return maxEle;
+    }
+
+    public int minFreq(int[] freq){
+        int minEle = Integer.MAX_VALUE;
+        for(int i = 0; i < freq.length; i++){
+            if(freq[i] != 0){
+                minEle = Math.min(minEle, freq[i]);
+            }
+        }
+        return minEle;
     }
 
     public int beautySum(String s) {
-        int n = s.length();
+        
+        int result = 0;
         int sum = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < s.length(); i++){
+            sum = 0;
             int[] freq = new int[26];
-            for(int j = i; j < n; j++){
-                int index = s.charAt(j) -'a';
+            for(int j = i; j < s.length(); j++){
+                int index = s.charAt(j) - 'a';
                 freq[index]++;
-                int beauty = maxFreq(freq)  - minFreq(freq);
-                sum += beauty;
+                sum = maxFreq(freq) - minFreq(freq);
+                result += sum;
             }
-            
         }
-        return sum;
+        return result;
     }
 }
