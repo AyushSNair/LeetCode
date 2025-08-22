6c1871bb795d9d9bd3428dd6ddfdfbcc5011481a
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-08-22 23:50:27 +0530

    Time: 3 ms (91.64%), Space: 44 MB (51.43%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,31 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int [] arr = new int[nums1.length];
-
-        for(int i = 0; i<nums1.length; i++){
-            boolean flag = false;
-            int index = 0;
-            for (int j = 0; j<nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    index = j;
-                    flag = true;
-                    break;
-                }
+        Stack<Integer> stk = new Stack<>();
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        int[] n = new int[nums1.length];
+        for(int i = 0; i < nums2.length; i++){
+            while(!stk.empty() && stk.peek() < nums2[i]){
+                hmap.put(stk.pop(), nums2[i]);
             }
-            boolean greaterFound = false;
-            for(int j = index + 1; j<nums2.length; j++){
-                if(nums2[j] > nums1[i]){
-                    arr[i] = nums2[j];
-                    greaterFound = true;
-                    break;
-                }
+            stk.push(nums2[i]);
+        }
 
-            }
 
-            if(greaterFound == false){
-                arr[i] = -1;
-            }
-        } 
-        return arr;
+        for(int i = 0; i < nums1.length; i++){
+            n[i] = hmap.getOrDefault(nums1[i] , -1);
+        }
+        return n;
     }
 }
