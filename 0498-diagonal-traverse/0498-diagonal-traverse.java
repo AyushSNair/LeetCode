9b14e54e9c175882423edabebd9dbdd1a2b740ee
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-08-27 14:26:23 +0530

    Time: 4 ms (40.49%), Space: 47.2 MB (65.12%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,42 +1,46 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        if (mat == null || mat.length == 0) return new int[0];
-
-        int m = mat.length, n = mat[0].length;
-        int[] result = new int[m * n];
-        int row = 0, col = 0, dir = 1; // 1 = up-right, -1 = down-left
-
-        for (int i = 0; i < m * n; i++) {
-            result[i] = mat[row][col];
-
-            // Moving up-right
-            if (dir == 1) {
-                if (col == n - 1) { // reached last column
-                    row++;
-                    dir = -1;
-                } else if (row == 0) { // reached first row
-                    col++;
-                    dir = -1;
-                } else {
-                    row--;
-                    col++;
-                }
+        int rowLength = mat.length; //no. of rows
+        int colLength = mat[0].length; // no of columns
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> arrList = new ArrayList<>();
+        int count = 0;
+        while(count != (rowLength * colLength)){
+            while(i >= 0 && j < colLength){
+                arrList.add(mat[i][j]);
+                count++;
+                i--;
+                j++;
             }
-            // Moving down-left
-            else {
-                if (row == m - 1) { // reached last row
-                    col++;
-                    dir = 1;
-                } else if (col == 0) { // reached first column
-                    row++;
-                    dir = 1;
-                } else {
-                    row++;
-                    col--;
-                }
+            if(j == colLength){
+                i += 2;
+                j = colLength - 1;
             }
+            else{
+                i++;
+            }
+            while(i < rowLength && j >=0 ){
+                arrList.add(mat[i][j]);
+                count++;
+                i++;
+                j--;
+            }
+            if( i == rowLength){
+                i--;
+                j += 2; 
+            }
+            else{
+                j++;
+            }    
+        }
+        int siz = arrList.size();
+        int[] arr = new int[siz];
+        for(int k = 0; k < siz; k++){
+            arr[k] = arrList.get(k);
         }
 
-        return result;
+        return arr;
+                
     }
 }
