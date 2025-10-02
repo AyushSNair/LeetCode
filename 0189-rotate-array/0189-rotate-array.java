4ebf79be4ca186eda82cb5efe04db9c664e78e12
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-10-02 23:30:42 +0530

    Time: 1 ms (45.42%), Space: 57.4 MB (54.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,37 +1,23 @@
-import java.util.*;
 class Solution {
-    public static void Reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;}}
-    public static void rotate(int[] nums, int k) {
+    public void rotate(int[] nums, int k) {
+
         int n = nums.length;
-        if (n == 1 || k == 0)
-            return; // No rotation needed
-        k = k % nums.length;
-        // Reverse the last k elements (from n-k to n-1)
-        Reverse(nums, n - k, n - 1);
-        // Reverse the first n-k elements (from 0 to n-k-1)
-        Reverse(nums, 0, n - k - 1);
-        // Reverse the entire array
-        Reverse(nums, 0, n - 1);}
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {nums[i] = sc.nextInt();}
-        int k = sc.nextInt();
-        if (n == 1 || k == 0) {
-            System.out.print("After Rotating the k elements to right: ");
-            for (int num : nums) {System.out.print(num + " ");}
-            return;}
-        k = k % n;
-        if (k > 0) {
-            rotate(nums, k);
+        k = k % n;          
+        if (k == 0) return; 
+        int[] arr = new int[nums.length];
+        int j = 0;
+        for(int i = nums.length - k  ; i < nums.length ; i++){
+            arr[j] = nums[i];
+            j++;
         }
-        System.out.print("After Rotating the k elements to right: ");
-        for (int num : nums) {
-            System.out.print(num + " ");}}}
+
+        for(int i = 0; i < nums.length - k; i++){
+            arr[j] = nums[i];
+            j++;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = arr[i];
+        }
+    }
+}
