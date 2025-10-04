dfdb2e7eb51c26c80d8ad0b2b5bbeaa1738b8d5c
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-10-04 05:39:15 +0530

    Time: 1 ms (99.45%), Space: 57.3 MB (31.94%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,12 +4,11 @@ class Solution {
         int sum = 0;
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            if(sum > max){ 
-                max = sum;
-            }
-            if(sum < 0 ){
+            max = Math.max(max, sum);
+            if(sum < 0){
                 sum = 0;
             }
+            
         }
         return max;
     }
