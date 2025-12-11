2d644eeb8b366b3f5e2c70c945d853384aa870d5
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-12-11 09:26:56 +0530

    Time: 0 ms (100%), Space: 42.5 MB (46.45%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,28 +1,20 @@
-import java.util.LinkedList;
-import java.util.Queue;
 class MyStack {
-        Queue<Integer> q1;
-        Queue<Integer> q2;
+    Queue<Integer> q1;
+    Queue<Integer> q2;
     public MyStack() {
         q1 = new LinkedList<>();
-        q2 = new LinkedList<>();        
+        q2 = new LinkedList<>();
     }
     
     public void push(int x) {
-        int size = q1.size();
-        if(q1.isEmpty()){
-            q1.offer(x);
+        while(!q1.isEmpty()){
+            q2.offer(q1.poll());
         }
-        else if(!q1.isEmpty() && q2.isEmpty()){
-            for(int i=1; i<=size; i++){
-                int temp = q1.poll();
-                q2.offer(temp);
-            }
-            q1.offer(x);
-            while(!q2.isEmpty()){
-                int temp = q2.poll();
-                q1.offer(temp);
-            }
+
+        q1.offer(x);
+
+        while(!q2.isEmpty()){
+            q1.offer(q2.poll());
         }
     }
     
