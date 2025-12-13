79a9b25d8d1ef2069fde8af36119429d9c880fa7
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-12-13 19:43:53 +0530

    Time: 1 ms (76.68%), Space: 43.7 MB (34.52%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,29 +15,28 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        //level order traversal but reverse arrayList at even position.
-
         List<List<Integer>> wrapList = new ArrayList<>();
-
-        newTraversal(root, wrapList);
+        TreeNode node = root;
+        zigzagFunc(wrapList, node);
 
         return wrapList;
     }
 
-    public List<List<Integer>> newTraversal(TreeNode node, List<List<Integer>> wrapList){
-
+    public List<List<Integer>> zigzagFunc(List<List<Integer>> wrapList, TreeNode node){
         if(node == null){
             return wrapList;
         }
+
         Queue<TreeNode> queue = new LinkedList<>();
+
+        
 
         queue.offer(node);
         int j = 1;
-
-        while(!queue.isEmpty()){
+        while(queue.size() != 0){
             int levelNum = queue.size();
-            List<Integer> arrList = new ArrayList<>();
-            
+            ArrayList<Integer> arrList = new ArrayList<>();
+
             for(int i = 0; i < levelNum; i++){
                 if(queue.peek().left != null){
                     queue.offer(queue.peek().left);
@@ -45,19 +44,21 @@ class Solution {
                 if(queue.peek().right != null){
                     queue.offer(queue.peek().right);
                 }
-                arrList.add(queue.poll().val);
 
+                arrList.add(queue.poll().val);
             }
+
             if(j % 2 == 0){
                 Collections.reverse(arrList);
                 wrapList.add(arrList);
-                j++;
             }
-            else if(j % 2 != 0){
+
+            if(j % 2 != 0){
                 wrapList.add(arrList);
-                j++;
             }
+            j++;
         }
+        
         return wrapList;
     }
 }
