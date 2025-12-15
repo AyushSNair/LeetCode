2227ff8d7530947c2a60ba14000edef5fc6a866d
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-12-15 16:51:13 +0530

    Time: 0 ms (100%), Space: 48.3 MB (11.33%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,34 +1,47 @@
 class Solution {
-    public static double myPow(double x, int n) {
-        double ans = 1.0;
+    public double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+
+        if(n == 1){
+            return x;
+        }
+        
+
         long nn = n;
+        if(nn < 0){
+            nn = -1 * nn;
+        }
+
+
+        double ans = powerFunc(x, nn);
 
         if(n < 0){
-            nn = (-1) * nn;
+            return 1/ans;
+        }else{
+            return ans;
         }
+    }
+
+    public double powerFunc(double x, long nn){
+        double ans  = 1;
         while(nn > 0){
-            if(nn % 2 == 0){
-                x = x*x;
-                nn = nn/2;
-            }
-            else{
+            if(nn % 2 == 1){
                 ans = ans * x;
                 nn = nn -1;
             }
-
-        }
-
-        if(n < 0){
-            ans = 1/ans;
+            else{
+                x = x * x;
+                nn = nn / 2;
+            }
         }
         return ans;
+
+        
+
+        
     }
 
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        double x =sc.nextInt();
-        int n = sc.nextInt();
-
-        System.out.print(myPow(x,n));
-    }
+    
 }
