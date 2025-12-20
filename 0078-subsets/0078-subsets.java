830efd181fdb3aded7c6afde8668b488dc19ea4e
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-12-20 18:32:38 +0530

    Time: 1 ms (79.74%), Space: 44 MB (67.68%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,41 +1,37 @@
 class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        ArrayList<Integer> ip = new ArrayList<>();
 
-    public List<List<Integer>> solve(ArrayList<Integer> ip, ArrayList<Integer> op, List<List<Integer>> arrList){
+        for(int i = 0; i < nums.length; i++){
+            ip.add(nums[i]);
+        }
+        
+        List<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> op = new ArrayList<>();
+
+        solutionFunc(result, op, ip, ip.size());
+
+        return result;
+    }
+    
+    public void solutionFunc(List<List<Integer>> result, ArrayList<Integer> op, ArrayList<Integer> ip, int n){
         if(ip.size() == 0){
-            arrList.add(new ArrayList<>(op));
-            return arrList;
+            result.add(new ArrayList<>(op));
+            return;
         }
 
         ArrayList<Integer> op1 = new ArrayList<>(op);
         ArrayList<Integer> op2 = new ArrayList<>(op);
-    
 
+        ArrayList<Integer> ip1 = new ArrayList<>(ip);
+        ArrayList<Integer> ip2 = new ArrayList<>(ip);
+        
         op2.add(ip.get(0));
-        ArrayList<Integer> newIp = new ArrayList<>(ip);
-        newIp.remove(0);
+        ip1.remove(0);
+        ip2.remove(0);
 
-        solve(newIp,op1,arrList);
-        solve(newIp,op2,arrList);
-
-        return arrList;
-    }
-
-    public List<List<Integer>> subsets(int[] nums) {
-        
-
-        List<List<Integer>> arrList = new ArrayList<>();
-        if(nums.length == 0){
-            return arrList;
-        }
-        
-
-        ArrayList<Integer> ip = new ArrayList<>();
-        for (int num : nums) {
-            ip.add(num);
-        }
-        ArrayList<Integer> op = new ArrayList<>();
-
-        solve(ip,op,arrList);
-        return arrList;
+        solutionFunc(result, op1, ip1, ip.size());
+        solutionFunc(result, op2, ip2, ip.size());
     }
 }
