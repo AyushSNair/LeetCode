3a9d25a4d388c877f45815477a797079e7ca49d7
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-12-21 13:31:37 +0530

    Time: 2 ms (85.65%), Space: 45.5 MB (6.53%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,46 +1,42 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> arrList = new ArrayList<>();
-        ArrayList<Integer> arr = new ArrayList<>();
         ArrayList<Integer> ip = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
             ip.add(nums[i]);
         }
+        
         ArrayList<Integer> op = new ArrayList<>();
-        solve(arrList, ip,op);
 
         List<List<Integer>> result = new ArrayList<>();
-        for(int i = 0; i < arrList.size(); i++){
-            if(result.contains(arrList.get(i))){
-                continue;
-            }
 
-            result.add(arrList.get(i));
-        }        
+        solutionFunc(result, ip, op );
 
         return result;
     }
 
-    public List<List<Integer>> solve(List<List<Integer>> arrList, ArrayList<Integer> ip,ArrayList<Integer> op){
+    public void solutionFunc(List<List<Integer>> result, ArrayList<Integer> ip, ArrayList<Integer> op){
         if(ip.size() == 0){
-            
-            arrList.add(op);
-            return arrList;
+            result.add(new ArrayList<>(op));
+            return;
         }
 
         ArrayList<Integer> op1 = new ArrayList<>(op);
         ArrayList<Integer> op2 = new ArrayList<>(op);
 
-        op2.add(ip.get(0));
-        ArrayList<Integer> newIp = new ArrayList<>(ip);
-        newIp.remove(0);
+        ArrayList<Integer> ip1 = new ArrayList<>(ip);
+        ArrayList<Integer> ip2 = new ArrayList<>(ip);
 
-        solve(arrList, newIp, op1);
-        solve(arrList, newIp, op2);
+        ip1.remove(0);
+        while(ip1.size() > 0 && ip1.get(0).equals(ip.get(0))){
+            ip1.remove(0);
+        }
+        solutionFunc(result, ip1, op1);
 
-        return arrList;
+        op2.add(ip2.get(0));
+        ip2.remove(0);
+        solutionFunc(result, ip2, op2);
+
     }
-
 }
