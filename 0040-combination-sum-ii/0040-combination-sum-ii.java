5a8179302dfdad3ddca46f422d08f162e7493369
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-12-21 12:01:12 +0530

    Time: 27 ms (13.52%), Space: 53 MB (5.48%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,45 +1,57 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList<>();
         ArrayList<Integer> ip = new ArrayList<>();
+        Arrays.sort(candidates);
+
         for(int i = 0; i < candidates.length; i++){
             ip.add(candidates[i]);
         }
 
         ArrayList<Integer> op = new ArrayList<>();
-        solve(ip, op, target, result);
+        List<List<Integer>> result = new ArrayList<>();
+        int sum = 0;
+
+        solutionFunc(result, ip, op, sum, target);
+
         return result;
+
     }
 
-    public List<List<Integer>> solve(ArrayList<Integer> ip, ArrayList<Integer> op, int target, List<List<Integer>> result){
-        if(target == 0){
-            if(result.contains(new ArrayList<>(op))){
-                return result;
-            }
+    public void solutionFunc(List<List<Integer>> result, ArrayList<Integer> ip, ArrayList<Integer> op, int sum, int target){
+        if(sum == target){
+            
             result.add(new ArrayList<>(op));
-            return result;
+            return;
         }
 
-        if(target < 0 || ip.size() == 0){
-            return result;
+        if(sum > target || ip.size() == 0){
+            return;
         }
 
-        for (int i = 0; i < ip.size(); i++) {
-            if (i > 0 && ip.get(i).equals(ip.get(i - 1))) continue; // Skip duplicates
+        ArrayList<Integer> ip1 = new ArrayList<>(ip);
+        
+        ArrayList<Integer> ip2 = new ArrayList<>(ip);
+        
+        ArrayList<Integer> op1 = new ArrayList<>(op);
+        
+        ArrayList<Integer> op2 = new ArrayList<>(op);
 
-            int current = ip.get(i);
-            if (current > target) break;
+        int sum1 = sum;
+        int sum2 = sum;
 
-            ArrayList<Integer> newOp = new ArrayList<>(op);
-            newOp.add(current);
-
-            // create a new list excluding the current element and everything before it
-            ArrayList<Integer> newIp = new ArrayList<>(ip.subList(i + 1, ip.size()));
-
-            solve(newIp, newOp, target - current, result);
+        ip1.remove(0);
+        while (ip1.size() > 0 && ip1.get(0).equals(ip.get(0))) {
+            ip1.remove(0);
         }
 
-        return result;
+        solutionFunc(result, ip1, op1, sum1, target);
+
+        op2.add(ip2.get(0));
+        ip2.remove(0);
+        
+
+        sum2 = sum2 + op2.get(op2.size() - 1);
+        solutionFunc(result, ip2, op2, sum2, target);
+
     }
 }
