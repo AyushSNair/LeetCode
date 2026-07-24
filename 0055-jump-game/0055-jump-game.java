a11e88bcaa9595c1a4c4316165ed6d995662bdf4
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2026-07-24 13:34:37 +0530

    Time: 3 ms (21.56%), Space: 48.1 MB (23.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        int n = nums.length;
+
         int maxIndex = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(i > maxIndex){
+
+        for(int i = 0; i < n; i++){
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+
+            if(i != n -1 && nums[i] == 0 && maxIndex <= i){
                 return false;
             }
-            if(nums[i] + i > maxIndex){
-                maxIndex = nums[i] + i;
-            }
-            if(maxIndex >= nums.length - 1){
-                return true;
-            }
         }
-        return false;
+
+        return true;
     }
 }
