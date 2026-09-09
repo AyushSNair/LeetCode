f65ceb47a0c25e6d9bd0a5ed651ba1d7fc759462
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2026-09-10 00:40:12 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (56.41%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long countCommas(long n) {
+        long total = 0;
+        long power = 1000;
+
+        while (power <= n) {
+            total += n - power + 1;
+            power *= 1000;
+        }
+
+        return total;        
+    }
+}
